6d68d1199305fe9c5844717708a11e783d8dbd47
Rens Saladaga <staleexam19@gmail.com>
2024-07-22 21:05:51 +0800

    feat: added essential pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './views/home-page/home-page.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
+import { SignInPageComponent } from './views/sign-in-page/sign-in-page.component';
+import { DashboardPageComponent } from './views/dashboard-page/dashboard-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: HomePageComponent
+    },
+    {
+        path: "signin",
+        component: SignInPageComponent
+    },
+    {
+        path: "dashboard",
+        component: DashboardPageComponent
+    },
+    {
+        path: "**",
+        component: PageNotFoundComponent
+    }
+];
