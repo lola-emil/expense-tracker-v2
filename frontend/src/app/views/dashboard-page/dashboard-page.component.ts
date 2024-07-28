b518a265eaed80b2b71f59ef8bf55705997e9ac6
Rens Saladaga <staleexam19@gmail.com>
2024-07-28 18:56:23 +0800

    feat: dashboard semi-complete

--- frontend/src/app/views/dashboard-page/dashboard-page.component.ts
+++ frontend/src/app/views/dashboard-page/dashboard-page.component.ts
@@ -3,6 +3,8 @@ import { InsightsChartComponent } from '../../components/insights-chart/insights
 import { TopNavComponent } from "../../components/top-nav/top-nav.component";
 import { SideNavComponent } from "../../components/side-nav/side-nav.component";
 import { StatComponent } from "../../components/stat/stat.component";
+import { NgxEchartsDirective } from 'ngx-echarts';
+import { EChartsOption } from 'echarts';
 
 @Component({
   selector: 'app-dashboard-page',
@@ -12,7 +14,8 @@ import { StatComponent } from "../../components/stat/stat.component";
     TopNavComponent,
     SideNavComponent,
     StatComponent,
-],
+    NgxEchartsDirective
+  ],
   templateUrl: './dashboard-page.component.html',
   styleUrl: './dashboard-page.component.css'
 })
@@ -20,5 +23,44 @@ export class DashboardPageComponent {
 
   constructor() { }
 
+  chartOptions: EChartsOption = {
+    tooltip: {
+      trigger: 'item'
+    },
 
-}
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        padAngle: 5,
+        itemStyle: {
+          borderRadius: 10
+        },
+        label: {
+          show: false,
+          position: 'center'
+        },
+        emphasis: {
+          label: {
+            show: false,
+            fontSize: 40,
+            fontWeight: 'bold'
+          }
+        },
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 1048, name: 'Search Engine' },
+          { value: 735, name: 'Direct' },
+          { value: 580, name: 'Email' },
+          { value: 484, name: 'Union Ads' },
+          { value: 300, name: 'Video Ads' }
+        ]
+      }
+    ]
+  };
+
+};
