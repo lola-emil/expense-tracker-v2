1ac72e442ad6490eeacbe9578b7fdcfdaf28e700
Rens Saladaga <staleexam19@gmail.com>
2024-09-02 23:05:14 +0800

    feat: added validation for file types and file size

--- backend/src/features/document-ai/controllers/parse-expense.controller.ts
+++ backend/src/features/document-ai/controllers/parse-expense.controller.ts
@@ -2,14 +2,25 @@ import { Request, Response } from "express";
 import { UploadedFile } from "express-fileupload";
 import { DocumentProcessorServiceClient } from "@google-cloud/documentai";
 import { PROCESSOR_ID, PROJECT_ID, PROJECT_LOCATION } from "../../../config/constants";
+import { ErrorResponse } from "../../../middlewares/errorhandler";
 
 
 export async function parseExpense(req: Request, res: Response) {
     const image = req.files!.image as UploadedFile;
+    const supportedTypes = ["application/pdf", "image/gif", "image/tiff", "image/jpeg", "image/png", "image/bmp", "image/webp", "text/html"];
 
-    /**
-     * TODO: need validation for file's mimeType
-     */
+    console.log(image.size > 2e+7);
+
+    // Check if file is valid
+    if (!supportedTypes.includes(image.mimetype))
+        throw new ErrorResponse(422, "", {
+            image: ["File type not supported"]
+        });
+
+    if (image.size > 2e+7)
+        throw new ErrorResponse(422, "", {
+            image: ["Maximum file size is 20MB"]
+        });
 
     const name = `projects/${PROJECT_ID}/locations/${PROJECT_LOCATION}/processors/${PROCESSOR_ID}`;
     const encodedImage = Buffer.from(image.data).toString("base64");
