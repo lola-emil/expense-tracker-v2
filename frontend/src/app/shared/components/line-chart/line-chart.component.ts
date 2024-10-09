c28bb9894cfd27aee0e84e7abe3a30a05ec44a26
Rens Saladaga <staleexam19@gmail.com>
2024-10-09 22:02:58 +0800

    feat: added base layout for dashboard

--- frontend/src/app/shared/components/line-chart/line-chart.component.ts
+++ frontend/src/app/shared/components/line-chart/line-chart.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { EChartsOption } from 'echarts';
 import { NgxEchartsDirective, NgxEchartsModule } from 'ngx-echarts';
-
+import { LinearGradientObject } from 'echarts';
 @Component({
   selector: 'app-line-chart',
   standalone: true,
@@ -11,7 +11,47 @@ import { NgxEchartsDirective, NgxEchartsModule } from 'ngx-echarts';
 })
 export class LineChartComponent {
 
-  chartOptions: EChartsOption = {};
+
+
+  chartOptions: EChartsOption = {
+    title: {
+      text: 'Basic Line Chart',
+    },
+    tooltip: {
+      trigger: 'axis',
+    },
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    },
+    yAxis: {
+      type: 'value',
+      splitLine: {
+        show: true, // Show or hide split lines
+        lineStyle: {
+          color: '#333', // Color of the split lines
+          width: 1,          // Width of the split lines
+          type: 'dashed',    // Type of the split lines ('solid', 'dashed', 'dotted')
+        },
+      }
+    },
+
+    grid: {
+      left: '10%',
+      right: '10%',
+      bottom: '10%',
+      containLabel: true, // Makes sure labels don't overlap with chart edges
+    },
+    series: [{
+      data: [120, 200, 150, 80, 70, 110, 130, 130],
+      type: 'line',
+      smooth: true, // Makes the line smooth instead of sharp
+      lineStyle: {
+        color: "#ff0000"
+      },
+
+    }],
+  };
 
 
 }
