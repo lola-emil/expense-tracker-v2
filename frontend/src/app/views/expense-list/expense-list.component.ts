ff4d118c04f710f5f4ea96e1f7c4a72df1a3fd3a
Rens Saladaga <staleexam19@gmail.com>
2024-10-18 21:14:39 +0800

    feat: expense list

--- frontend/src/app/views/expense-list/expense-list.component.ts
+++ frontend/src/app/views/expense-list/expense-list.component.ts
@@ -1,12 +1,107 @@
 import { Component } from '@angular/core';
 import { UserLayoutComponent } from "../../shared/layout/user-layout/user-layout.component";
+import { CurrencyPipe } from "@angular/common";
 
 @Component({
   selector: 'app-expense-list',
   standalone: true,
-  imports: [UserLayoutComponent],
+  imports: [UserLayoutComponent, CurrencyPipe],
   templateUrl: './expense-list.component.html'
 })
 export class ExpenseListComponent {
+  recentActivities = [
+    {
+      id: 1,
+      transactionNumber: "TXN001",
+      amount: 1000,
+      vendor: "Office Depot",
+      date: "2024-10-01",
+      status: "Pending"
+    },
+
+    {
+      id: 1,
+      transactionNumber: "TXN002",
+      amount: 399.99,
+      vendor: "Uber",
+      date: "2024-10-01",
+      status: "Pending"
+    },
+
+    {
+      id: 1,
+      transactionNumber: "TXN003",
+      amount: 12.50,
+      vendor: "Starbucks",
+      date: "2024-10-01",
+      status: "Pending"
+    },
+
+    {
+      id: 1,
+      transactionNumber: "TXN004",
+      amount: 423.25,
+      vendor: "Amazon",
+      date: "2024-10-01",
+      status: "Pending"
+    },
+
+    {
+      id: 1,
+      transactionNumber: "TXN005",
+      amount: 450.00,
+      vendor: "Airbnb",
+      date: "2024-10-01",
+      status: "Pending"
+    },
+
+    {
+      id: 1,
+      transactionNumber: "TXN001",
+      amount: 1000,
+      vendor: "Office Depot",
+      date: "2024-10-01",
+      status: "Pending"
+    },
+
+    {
+      id: 1,
+      transactionNumber: "TXN002",
+      amount: 399.99,
+      vendor: "Uber",
+      date: "2024-10-01",
+      status: "Pending"
+    },
+
+    {
+      id: 1,
+      transactionNumber: "TXN003",
+      amount: 12.50,
+      vendor: "Starbucks",
+      date: "2024-10-01",
+      status: "Pending"
+    },
+
+    {
+      id: 1,
+      transactionNumber: "TXN004",
+      amount: 423.25,
+      vendor: "Amazon",
+      date: "2024-10-01",
+      status: "Pending"
+    },
+
+    {
+      id: 1,
+      transactionNumber: "TXN005",
+      amount: 450.00,
+      vendor: "Airbnb",
+      date: "2024-10-01",
+      status: "Pending"
+    },
+
+  ];
+
+  totalExpense = 823.63;
 
 }
